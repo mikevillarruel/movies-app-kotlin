00b0025796433f4b1fbdbe0000ce33471e5375e6
Michael Villarruel <alejovillarruel@gmail.com>
2021-09-19 19:17:14 -0500

    ViewBinding

--- app/src/main/java/com/example/moviesapp/MovieDetailFragment.kt
+++ app/src/main/java/com/example/moviesapp/MovieDetailFragment.kt
@@ -1,15 +1,17 @@
 package com.example.moviesapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.moviesapp.databinding.FragmentMovieDetailBinding
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
 
+    private lateinit var binding: FragmentMovieDetailBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieDetailBinding.bind(view)
     }
 
 }
