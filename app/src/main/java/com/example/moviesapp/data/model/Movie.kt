03f4f3e7e9b07877be7b4a9062798aa228b9a56b
Michael Villarruel <alejovillarruel@gmail.com>
2021-09-20 21:04:30 -0500

    data layer

--- app/src/main/java/com/example/moviesapp/data/model/Movie.kt
+++ app/src/main/java/com/example/moviesapp/data/model/Movie.kt
@@ -1,4 +1,23 @@
 package com.example.moviesapp.data.model
 
-data class Movie(val id: Int = -1) {
-}
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+    val id: Int = -1,
+    // @SerializedName("adult")
+    val adult: Boolean = false,
+    val backdropPath: String = "",
+    val genreIDS: List<Int> = listOf(),
+    val originalLanguage: String = "",
+    val originalTitle: String = "",
+    val overview: String = "",
+    val popularity: Double = -1.0,
+    val posterPath: String = "",
+    val releaseDate: String = "",
+    val title: String = "",
+    val video: Boolean = false,
+    val voteAverage: Double = -1.0,
+    val voteCount: Long = -1
+)
+
+data class MovieList(val results: List<Movie> = listOf())
--- app/src/main/java/com/example/moviesapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/example/moviesapp/data/remote/MovieDataSource.kt
@@ -1,4 +1,18 @@
 package com.example.moviesapp.data.remote
 
+import com.example.moviesapp.data.model.MovieList
+
 class MovieDataSource {
+
+    fun getUpcomingMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getTopRatedMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getPopularMovies(): MovieList {
+        return MovieList()
+    }
 }
