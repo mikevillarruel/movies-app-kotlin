7302432599f88b4f8c6a6f7b19e225416d03bbc9
Michael Villarruel <alejovillarruel@gmail.com>
2021-09-20 23:24:52 -0500

    repository layer

---
+++ app/src/main/java/com/example/moviesapp/aplication/AppConstants.kt
@@ -0,0 +1,9 @@
+package com.example.moviesapp.aplication
+
+object AppConstants {
+    // API
+    const val BASE_URL = "https://api.themoviedb.org/3"
+
+    // API KEY
+    const val API_KEY = "f27f2aa47421a737b6e0e59c52c0fd89"
+}
--- app/src/main/java/com/example/moviesapp/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/moviesapp/repository/MovieRepositoryImpl.kt
@@ -1,4 +1,14 @@
 package com.example.moviesapp.repository
 
-class MovieRepositoryImpl : MovieRepository {
+import com.example.moviesapp.data.model.MovieList
+import com.example.moviesapp.data.remote.MovieDataSource
+
+class MovieRepositoryImpl(private val dataSource: MovieDataSource) : MovieRepository {
+
+    override suspend fun getUpcomingMovies(): MovieList = dataSource.getUpcomingMovies()
+
+    override suspend fun getTopRatedMovies(): MovieList = dataSource.getTopRatedMovies()
+
+    override suspend fun getPopularMovies(): MovieList = dataSource.getPopularMovies()
+
 }
