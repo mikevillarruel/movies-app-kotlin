d4b00b0b49f08ebdd7bbd28d559249981f37951c
Michael Villarruel <alejovillarruel@gmail.com>
2021-09-23 00:38:19 -0500

    entity and dao

---
+++ app/src/main/java/com/example/moviesapp/data/local/MovieDao.kt
@@ -0,0 +1,18 @@
+package com.example.moviesapp.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.moviesapp.data.model.MovieEntity
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM movieentity")
+    suspend fun getAllMovies(): List<MovieEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveMovie(movieEntity: MovieEntity)
+
+}
